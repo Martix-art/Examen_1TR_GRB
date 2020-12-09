2a82a3540f57237d381d9a3a4524599b98a8a6ef
Martix-art <leo.enc.marta@gmail.com>
2020-12-09 20:08:09 +0100

    he intentado los textos

--- Marta Leonardo/Assets/Scripts/shapeship.cs
+++ Marta Leonardo/Assets/Scripts/shapeship.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; //Importante importar esta librería para usar la UI
 
 public class shapeship : MonoBehaviour
 {
     public float speed;
     private float translation;
+    private Vector3 moveSpeed;
 
-    // private float moveSpeed = 3f;
+    //Variable que determina cómo de rápido se mueve la nave con el joystick
+    private float moveSpeed = 3f;
 
+    //Capturo el texto del UI que indicará la distancia recorrida
+    [SerializeField] Text TextDistance;
 
+    //Variable para parar el juego
+    [SerializeField] MeshRenderer myMesh;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,35 +30,57 @@ public class shapeship : MonoBehaviour
         MoverNave();
     }
 
-    void MoverNave()
+    //codigo para la colision
+    private void OnTriggerEnter(Collider other)
     {
-        /*
-        //Ejemplos de Input que podemos usar más adelante
-        if(Input.GetKey(KeyCode.Space))
+        if (other.gameObject.tag == "obstacle")
         {
-            print("Se está pulsando");
+            //desaparece la maya
+            myMesh.enabled = false;
+            //la velocidad se para
+            speed = 0f;
+            //la corutine se para
+            StopCoroutine("Distancia");
         }
 
-        if(Input.GetButtonDown("Fire1"))
+    }
+
+    //Corrutina que hace cambiar el texto de tiempo transcurrido
+    IEnumerator Distancia()
+    {
+        //Bucle infinito que suma 10 en cada ciclo
+        //El segundo parámetro está vacío, por eso es infinito
+        for (int n = 0; ; n += 1)
         {
-            print("Se está disparando");
+            //Cambio el texto que aparece en pantalla
+            TextDistance.text = "DISTANCIA: " + n * speed;
+
+            //cada ciclo aumenta la velocidad
+            if (speed < 26)
+            {
+                speed = speed + 0.3f;
+            }
+
+            //Ejecuto cada ciclo esperando 1 segundo
+            yield return new WaitForSeconds(0.25f);
         }
-        */
+
+    }
+    void MoverNave()
+    {
+        
         //Variable float que obtiene el valor del eje horizontal y vertical
         float desplX = Input.GetAxis("Horizontal") * speed;
-        transform.Translate(Vector3.left * Time.deltaTime * desplX);
+        transform.Translate(Vector3.right * Time.deltaTime * desplX);
        
 
         float desplY = Input.GetAxis("Vertical") * speed;
         transform.Translate(Vector3.up * Time.deltaTime * desplY);
 
-       
-
-        // transform.Translate(0, 0, translation);
-
+      
         //Movemos la nave mediante el método transform.translate
         //Lo multiplicamos por deltaTime, el eje y la velocidad de movimiento la nave
-        // transform.Translate(Vector3.right * Time.deltaTime * moveSpeed * desplX);
-        //  transform.Translate(Vector3.up * Time.deltaTime * moveSpeed * desplY);
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed * desplX);
+        transform.Translate(Vector3.up * Time.deltaTime * moveSpeed * desplY);
     }
 }
