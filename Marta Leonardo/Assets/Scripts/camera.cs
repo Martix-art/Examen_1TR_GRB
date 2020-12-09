37f6d77db7cb03033dece5677e31a42c5cac5341
Martix-art <leo.enc.marta@gmail.com>
2020-12-09 18:53:34 +0100

    movimiento nave
    he intentado el movimiento de la nave, ahora a ver si le sigue la camra

---
+++ Marta Leonardo/Assets/Scripts/camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    [SerializeField] Transform Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Estas líneas de código hacen que la cámara siga al objeyivo
+        transform.position = new Vector3(Target.position.x, Target.position.y + 1.5f, Target.position.z - 3);
+    }
+}
---
+++ Marta Leonardo/Assets/Scripts/shapeship.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shapeship : MonoBehaviour
+{
+    public float speed;
+    private float translation;
+
+    // private float moveSpeed = 3f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Ejecutamos la función propia que permite mover la nave con el joystick
+        MoverNave();
+    }
+
+    void MoverNave()
+    {
+        /*
+        //Ejemplos de Input que podemos usar más adelante
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("Se está pulsando");
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            print("Se está disparando");
+        }
+        */
+        //Variable float que obtiene el valor del eje horizontal y vertical
+        float desplXleft = Input.GetAxis("Horizontal") * speed;
+        transform.Translate(Vector3.left * Time.deltaTime * desplXleft);
+        float desplXright = Input.GetAxis("Horizontal") * speed;
+        transform.Translate(Vector3.left * Time.deltaTime * desplXright);
+
+        float desplY = Input.GetAxis("Vertical") * speed;
+        transform.Translate(Vector3.up * Time.deltaTime * desplY);
+
+        float translation = Input.GetAxis ("Vertical") * speed;
+
+        translation *= Time.deltaTime;
+        
+        transform.Translate(0, 0, translation);
+
+        // transform.Translate(0, 0, translation);
+
+        //Movemos la nave mediante el método transform.translate
+        //Lo multiplicamos por deltaTime, el eje y la velocidad de movimiento la nave
+        // transform.Translate(Vector3.right * Time.deltaTime * moveSpeed * desplX);
+        //  transform.Translate(Vector3.up * Time.deltaTime * moveSpeed * desplY);
+    }
+}
